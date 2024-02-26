5142b0862066d5f818e4571a678c858e9ec6727a
ManikantaGunda <82748008+ManikantaGunda@users.noreply.github.com>
2024-02-26 15:49:47 +0530

    Add files via upload

---
+++ Dff_tb.v
@@ -0,0 +1,16 @@
+module Dff_tb;
+reg a,rst,c;
+wire d;
+parameter k=5;
+MultiConfigDFlipFlop #(k)M(rst,c,a,d);
+always #5 c=~c;
+always #3 a=~a;
+always #6 rst=~rst;
+initial begin
+c=0;
+rst=1; a=0;
+
+#80 $finish;
+end
+endmodule
+
